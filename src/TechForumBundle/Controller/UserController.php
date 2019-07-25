bed685564260cb43b2bfffb4ae31cec2c6231f9a
GeorgiArn <georgearnaudov1@gmail.com>
2019-07-25 22:30:10 +0300

    Inser user functionality

--- src/TechForumBundle/Controller/UserController.php
+++ src/TechForumBundle/Controller/UserController.php
@@ -5,6 +5,8 @@ namespace TechForumBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use TechForumBundle\Entity\User;
+use TechForumBundle\Form\UserType;
 
 class UserController extends Controller
 {
@@ -15,7 +17,25 @@ class UserController extends Controller
      */
     public function register(Request $request)
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-        return $this->render('users/register.html.twig');
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $passwordHash = $this
+                ->get('security.password_encoder')
+                ->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($passwordHash);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('security_login');
+        }
+
+        return $this->render('users/register.html.twig',
+            ['form' => $form->createView()]);
     }
 }
