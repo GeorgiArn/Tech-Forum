cbdfa6e01f191a38b92d9141ed2c2259701abb09
GeorgiArn <georgearnaudov1@gmail.com>
2019-08-06 18:40:18 +0300

    Add confirm password field

--- src/TechForumBundle/Form/UserType.php
+++ src/TechForumBundle/Form/UserType.php
@@ -5,6 +5,7 @@ namespace TechForumBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +21,12 @@ class UserType extends AbstractType
             ->add('email', EmailType::class)
             ->add('fullName', TextType::class)
             ->add('username', TextType::class)
-            ->add('password', TextType::class);
+            ->add('password', RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'first_options' => ['label' => 'Password'],
+                    'second_options' => ['label' => 'Confirm Password'],
+                ]);
     }
 
     /**
